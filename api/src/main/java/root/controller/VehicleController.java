5f45a6311ebb42e349965306991401d1cb0c6327
akhilesh bollam <bollamakhilesh@gmail.com>
2017-06-26 02:49:38 -0400

    feat(module-api): add logic for alert creation

--- api/src/main/java/root/controller/VehicleController.java
+++ api/src/main/java/root/controller/VehicleController.java
@@ -1,15 +1,12 @@
 package root.controller;
 
 
+import org.springframework.web.bind.annotation.*;
 import root.entity.Reading;
 import root.entity.Vehicle;
 import root.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,11 +16,13 @@ public class VehicleController {
     VehicleService service;
 
     @RequestMapping(method = RequestMethod.PUT, value = "vehicles", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @CrossOrigin
     public List<Vehicle> loadVehicles(@RequestBody List<Vehicle> vehicles){
         return service.loadVehicles(vehicles);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "readings", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @CrossOrigin
     public Reading ingestReading(@RequestBody Reading reading){
         return service.ingestReading(reading);
     }
